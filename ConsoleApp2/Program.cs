b42901373558a88b771e82c60bdbd47553f890e7
KiraR11 <123009064+KiraR11@users.noreply.github.com>
2023-09-18 16:33:07 +0700

    версия 1.1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,7 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
-            Console.WriteLine("люблю");
+            Console.WriteLine("не люблю");
         }
     }
 }
