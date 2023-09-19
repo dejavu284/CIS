373ca3cd5f8681b65cfba945da4fd308dbc77b66
dejavu284 <skuratov.1@mail.ru>
2023-09-19 11:38:20 +0700

    переделал класс филм

--- ConsoleApp2/Classes/Film.cs
+++ ConsoleApp2/Classes/Film.cs
@@ -8,14 +8,18 @@ namespace ConsoleApp2.Classes
 {
     internal class Film
     {
-        public Film(string name,string description, string genre) {
+        /*public Film(string name,string description, List<string> genre, List<string> actors) {
             this.name = name;
             this.description = description; 
-            this.genre = genre; 
-        }
-        private string name;
-        private string description;
-        private string genre;
+            this.genre = genre;
+            this.actors = actors;
+        }*/
+
+
+        public string name { get; }
+        public string description { get; }
+        public List<string> actors { get; }
+        public List<string> genre { get; }
 
 
     }
