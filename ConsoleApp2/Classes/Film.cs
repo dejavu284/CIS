48a2da3281272d059455bec702a93175f56e896a
KiraR11 <123009064+KiraR11@users.noreply.github.com>
2023-09-19 12:07:34 +0700

    метод вывода

--- ConsoleApp2/Classes/Film.cs
+++ ConsoleApp2/Classes/Film.cs
@@ -8,14 +8,18 @@ namespace ConsoleApp2.Classes
 {
     internal class Film
     {
-        public Film(string name,string description, string genre) {
+        public Film(string name, string description, string genre, int year) {
             this.name = name;
-            this.description = description; 
-            this.genre = genre; 
+            this.description = description;
+            this.genre = genre;
+            this.year = year;
         }
         private string name;
         private string description;
         private string genre;
+        private int year;
+
+        public string Name { get { return name; } }
 
     }
 }
