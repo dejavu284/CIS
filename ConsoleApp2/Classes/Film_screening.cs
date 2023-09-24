bc8f5289fc9f96f5081d67cd411d2c281a4017ca
dejavu284 <skuratov.1@mail.ru>
2023-09-24 18:17:33 +0700

    поудалял фигню

--- ConsoleApp2/Classes/Film_screening.cs
+++ ConsoleApp2/Classes/Film_screening.cs
@@ -1,5 +1,4 @@
-﻿using ConsoleApp2.Classes.CinemaHall;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,16 +8,14 @@ namespace ConsoleApp2.Classes
 {
     internal class Film_screening
     {
-        public Film_screening(Film film, DateOnly data, TimeOnly time, ICinemaHall hall)
+        public Film_screening(Film film, DateOnly data, TimeOnly time)
         {
             this.film = film;
             this.data = data;
             this.time = time;
-            this.hall = hall;
         }
         public Film film;
         public DateOnly data;
         public TimeOnly time;
-        public ICinemaHall hall;
     }
 }
