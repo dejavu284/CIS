df04ef37a822f52efdf2c3023c6accec333c0822
KiraR11 <123009064+KiraR11@users.noreply.github.com>
2023-11-28 13:21:42 +0700

    написал тесты на класс Basket

--- CIS.Tests/Tests.cs
+++ CIS.Tests/Tests.cs
@@ -8,7 +8,7 @@ namespace CIS.Tests
     public class Tests
     {
         /*классы для тестирования:
-            Basket, Seating(протестированно), Schedule*/
+            Basket, Seating(протестированно), Schedule(протестированно)*/
         [Fact]
         public void Booking_the_right_place()
         {
@@ -131,7 +131,6 @@ namespace CIS.Tests
             });
 
             Schedule scheduleForFilmOneTest = scheduleAll.FindByDate(new DateOnly(2023,10,2));
-
             Assert.Equal(scheduleForFilmOneTest.Shows.Count, scheduleForFilmOne.Shows.Count);
             Assert.Equal(scheduleForFilmOneTest.Shows[0].Name, scheduleForFilmOne.Shows[0].Name);
             Assert.Equal(scheduleForFilmOneTest.Shows[1].Name, scheduleForFilmOne.Shows[1].Name);
@@ -158,6 +157,32 @@ namespace CIS.Tests
             bool result = schedule.IsNull();
 
             Assert.True(result);
+        }
+        [Fact]
+        public void Add_Ticket_in_Basket()
+        {
+            Basket basket= new Basket();
+
+            int[][] places = new int[4][];
+            places[0] = new int[4] { 1, 2, 3, 4 };
+            places[1] = new int[4] { 5, 6, 7, 8 };
+            places[2] = new int[4] { 9, 10, 11, 12 };
+            places[3] = new int[4] { 13, 14, 15, 16 };
+
+            Seating seating = new Seating(places, 1);
+
+            Place placeForBooking = new Place(0, 0, 100);
+
+            Ticket ticket = new Ticket(
+                1,
+                new Show("фильм 1", new DateOnly(2023, 10, 2), new TimeOnly(12, 12), seating, 1),
+                placeForBooking
+                );
+
+            basket.AddTicket(ticket);
+
+            Assert.Same(basket.Tickets[0], ticket);
+            Assert.Equal(basket.Price,placeForBooking.Price);
         }
     }
 }
