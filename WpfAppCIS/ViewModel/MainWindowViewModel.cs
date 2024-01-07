42597102d79ad2d7d53e40015f8ab1a4c76261af
KiraR11 <123009064+KiraR11@users.noreply.github.com>
2024-01-08 01:36:45 +0700

    Удалил не нужные пакеты

--- WpfAppCIS/ViewModel/MainWindowViewModel.cs
+++ WpfAppCIS/ViewModel/MainWindowViewModel.cs
@@ -1,13 +1,5 @@
 ﻿using CinemaModel;
 using GalaSoft.MvvmLight.Command;
-using GMap.NET.WindowsForms;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Input;
 using WpfAppCIS.Data;
 using WpfAppCIS.Model;
